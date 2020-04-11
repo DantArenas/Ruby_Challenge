fdf0568a013feee2bd5ea34bba163a99eda3f677
Leyend <leyend.experience@gmail.com>
2020-04-10 21:44:29 -0500

    Update client with testing functionalities

--- client/lib/client.rb
+++ client/lib/client.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'socket'
+require 'objspace'
 
 class Client
 
@@ -25,7 +26,20 @@ class Client
       Thread.new do
         loop do
           message = $stdin.gets.chomp
-          @socket.puts message
+
+          if message == 'add'
+            line = 'add 12345 0 60 19 noreplay'
+            puts "shortcut ussed... sending: #{line} "
+            @socket.puts line
+          elsif message == 'tigres'
+            @socket.puts 'Tres tristes tigres'
+          elsif message == 'cas'
+            line = 'cas 12345 0 180 15 321 noreplay'
+            print "shortcut ussed... sending: #{line} "
+            @socket.puts line
+          else
+            @socket.puts message
+          end
         end
       end
     rescue IOError => e
@@ -53,11 +67,13 @@ class Client
   end
 
   def handle_message(message)
-    if message.include? "Clossing"
+    if message.include? 'SEND DATA'
+      puts "Storage command accpeted. To continue #{message}"
+    elsif message.include? 'Clossing'
       close_client
-    elsif message.include? "Server:"
+    elsif message.include? 'Server:'
       puts message
-    elsif message.include? "Server Says:"
+    elsif message.include? 'Server Says:'
       puts message
       puts "You answered: I'm fine, thanks! How are you?"
       @socket.puts "I'm fine, thanks! How are you?"
@@ -81,6 +97,25 @@ end
 # ===================================================
 # ===           HERE WE LAUNCH THE CLIENT         ===
 # ===================================================
+
+=begin
+    line = 'Tres tristes tigres'
+    data = ["Tres tristes tigres", "comen trigo en el trigal"]
+    data_size = ObjectSpace.memsize_of(data)
+
+    def array_bytesize(array)
+      byte_size = 0
+      array.each do |string|
+        byte_size += string.bytesize if (string != nil && string != "")
+      end
+      byte_size
+    end
+
+    byte_size = array_bytesize(data)
+
+    puts "data obj size = #{data_size} vs bytes size = #{byte_size}"
+    puts "tigres size = #{line.bytesize}"
+=end
 
 socket = TCPSocket.open('localhost', 8080)
 Client.new(socket)
